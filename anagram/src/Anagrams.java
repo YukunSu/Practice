90ebbe0f1d4842ee08481607befa04a98d747016
YukunSu <yu.k.su@mail.mcgill.ca>
2014-08-20 01:46:23 -0400

    add factorial calculation

--- anagram/src/Anagrams.java
+++ anagram/src/Anagrams.java
@@ -52,4 +52,10 @@ public class Anagrams {
     }
 
     // verify size
+    public int factorial(int number) {
+        if (number == 0 || number == 1) {
+            return 1;
+        }
+        return number * factorial (number - 1);
+    }
 }
